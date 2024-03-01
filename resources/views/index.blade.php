e9103be16305f074a81be89f808f1b3481600fa8
Filip Ludwiczak <filip-ludwiczak@wp.pl>
2024-03-01 11:11:19 +0100

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'The list of tasks')
 
 @section('content')
+  <div>
+    <a href="{{route('tasks.create')}}">Add Task!</a>
+  </div>
+
   @forelse ($tasks as $task)
     <div>
       <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
@@ -10,4 +14,10 @@
   @empty
     <div>There are no tasks!</div>
   @endforelse
+
+  @if ($tasks->count())
+    <div>
+      {{ $tasks->links() }}
+    </div>
+  @endif
 @endsection  
--- routes/web.php
+++ routes/web.php
@@ -57,3 +57,8 @@ Route::delete('/tasks/{task}', function (Task $task) {
     $task->delete();
     return redirect()->route('tasks.index')->with('success','Task deleted successfully!');
 })->name('tasks.destroy');
+
+Route::put('tasks/{task}/toggle-complete', function(Task $task){
+    $task->toggleComplete();
+    return redirect()->back()->with('success','Task updated succesfully!');
+})->name('task.toggle-complete');
